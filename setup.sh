ee141b9c5502532a5317fb8247dec66be712753c
Annakan <Annakan@users.noreply.github.com>
2015-02-01 22:40:43 +0100

    starting to isolate taiga datas in a subdirectory data/taiga

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-sudo mkdir -p /data/postgresql
+sudo mkdir -p /data/taiga/postgresql
 
-sudo docker run -d --name postgres    -p 5432:5432  -v /data/postgresql:/var/lib/postgresql/data postgres
+sudo docker run -d --name postgres    -p 5432:5432  -v /data/taiga/postgresql:/var/lib/postgresql/data postgres
 sudo docker run -d --name taiga-back  -p 8001:8001  --link postgres:postgres ipedrazas/taiga-back
 sudo docker run -d --name taiga-front -p 80:80 -p 8000:8000 --link taiga-back:taiga-back ipedrazas/taiga-front
 
